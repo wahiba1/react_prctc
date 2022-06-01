7557c78abe0ffbbba00ad6ae4cffac0c59b0eca9
Wahiba Akram <wahibaakram8@gmail.com>
2022-06-01 13:17:08 +0500

    composing react components

---
+++ helloreact/src/index.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import './index.css';
+// import App from './App';
+
+
+  // React.createElement("div", {style: {color: "purple"}}, React.createElement("h1",null, "hi!")),
+  // ReactDOM.render(
+  //   React.createElement(
+  //     "ul",
+  //     {style: {color: "blue"}},
+  //     React.createElement("li", null, "Hot Dogs"),
+  //     React.createElement("li", null, "Hamburger"),
+  //     React.createElement("li", null, "Pizza"),
+  //     React.createElement("li", null, "Sushi")
+  //   ),
+  //   document.getElementById("root")
+  // );
+
+
+  // ReactDOM.render(
+  //   <ul>
+  //     <li>Hot Dogs</li>
+  //     <li>Sushi</li>
+  //     <li>Hamburger</li>
+  //     <li>Pizza</li>
+  //   </ul>,
+  //   document.getElementById("root")
+  // )
+
+  // let city = "Madrid";
+  // let city = {
+  //   name: "Madrid",
+  //   country: "Spain"
+  // }
+  // ReactDOM.render(
+  //   <h1 id="heading" className="cool-text">Hello from {city.name} is in {city.country}!</h1>,
+  //   document.getElementById("root")
+  // )
+  // function Hello() {
+  //   return (
+  //     <div>
+  //       <h1>Welcome to React!</h1>
+  //       <p>Let's build something cool.</p>
+  //     </div>
+  //   )
+  // }
+  // ReactDOM.render(
+  //   <Hello />,
+  //   document.getElementById("root") 
+  // )
+  
+  // props:-
+  // function Hello(props) {
+    // console.log(props);
+  //   function Hello({library, message, number}) {
+  //   // console.log(Object.keys(props))
+
+  //     return (
+  //     <div>
+  //       {/* <h1>Welcome to {props.library}!</h1>
+  //       <p>{props.message}</p> */}
+  //       <h1>Welcome to {library}!</h1>
+  //       <p>{message}</p>
+  //       {/* <p>{props.number} Props Total</p> */}
+  //     {/* <p>{Object.keys(props).length}Props Total</p> */}
+  //     <p>{number}Props Total</p>
+  //     </div>
+  //   );
+  //  }
+  // ReactDOM.render(
+  //   <Hello 
+  //   library="React" 
+  //   message="Have fun!" 
+  //   number={3}/>,
+  //   document.getElementById("root") 
+  // )
+function Lake({name}) {
+  return <h1>{ name}</h1>;
+}
+  function App() {
+    return (
+    <div>
+      <Lake name="Lake Tahoe" />
+      <Lake name="Angora Lake" />
+      <Lake name="Shirley Lake" />
+    </div>
+    );
+  }
+  ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+  );
